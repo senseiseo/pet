e7a912a935cd5968282d2166a557129db52f07a3
ildar <1919mur@gmail.com>
2020-09-02 15:12:28 +0300

    no message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end 
 
+  def create
+    @user = User.new(user_params)    # Не окончательная реализация!
+    if @user.save
+      # Обработать успешное сохранение.
+    else
+      render 'new'
+    end
+  end 
+
+
+
+
+  private 
+
+  def user_params 
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
+
 end
