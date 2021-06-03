94ae9b58ae967f414145f24399c988e3d2faefd0
senseiseo <1919mur@gmail.com>
2021-06-03 21:53:45 +0300

    Добавлена модель вопросов

---
+++ AskIt/app/models/question.rb
@@ -0,0 +1,9 @@
+class Question < ApplicationRecord
+    validates :title , presence: true, length: {minimum:3}
+    validates :body ,  presence: true, length: {minimum:3}
+
+    def formatted_ctreated_at 
+        created_at.strftime('%Y-%m-%d %H:%M:%S')
+    end 
+
+end 
