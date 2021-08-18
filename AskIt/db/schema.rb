8b8d9684a3022e5acd2fb78f01f509b117be94bf
senseiseo <36659596+senseiseo@users.noreply.github.com>
2021-08-18 21:44:44 +0300

    добавлена модель юзер, и бскрипт для хеширования паролья

--- AskIt/db/schema.rb
+++ AskIt/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_180200) do
+ActiveRecord::Schema.define(version: 2021_08_18_183824) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2021_08_17_180200) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
