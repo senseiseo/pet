5b22e56bbe423ce66f16b20460d2df87ad6e7e1e
senseiseo <1919mur@gmail.com>
2021-09-14 22:30:16 +0300

    Добавлена проверка пароля, проверка почты

--- AskIt/app/models/user.rb
+++ AskIt/app/models/user.rb
@@ -1,4 +1,30 @@
 class User < ApplicationRecord
-  has_secure_password 
-  validates :email , presence: true, uniqueness: true 
+  attr_accessor :old_password
+
+  has_secure_password validations: false 
+
+  validate :correct_old_password, on: :update, if: -> { password.present? }
+  validate :password_presense 
+  validates :password , confirmation: true, allow_blank: true, length: {minimum: 2, maximum: 70}
+  validate :password_complexity 
+  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
+
+  private 
+
+  def correct_old_password
+    return if BCrypt::Password.new(password_digest_was).is_password?(old_password)
+
+    errors.add :old_password, 'is incorrect'
+  end
+
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+    errors.add :password, 'complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
+  def password_presense 
+    errors.add(:password, :blank) unless password_digest.present?
+  end 
 end
